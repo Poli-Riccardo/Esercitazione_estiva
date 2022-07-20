6c031fbd8682645263ec53d188490038c3150415
Riccardo Poli <109282312+Poli-Riccardo@users.noreply.github.com>
2022-07-20 13:52:30 +0200

    Add files via upload

---
+++ Cinema_V1/cmake-build-debug/CMakeFiles/Cinema_V1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cinema_V1.dir/main.cpp.obj"
+  "Cinema_V1.exe"
+  "Cinema_V1.exe.manifest"
+  "Cinema_V1.pdb"
+  "libCinema_V1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cinema_V1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
