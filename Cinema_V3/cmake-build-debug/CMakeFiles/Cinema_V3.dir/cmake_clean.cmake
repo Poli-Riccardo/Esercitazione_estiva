00ef6c1b71de95bece482017bd4e8cfcb62ddfce
Riccardo Poli <109282312+Poli-Riccardo@users.noreply.github.com>
2022-07-26 15:15:24 +0200

    Add files via upload
    Aggiunta la funzione di stampare il biglietto

---
+++ Cinema_V3/cmake-build-debug/CMakeFiles/Cinema_V3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cinema_V3.dir/main.cpp.obj"
+  "Cinema_V3.exe"
+  "Cinema_V3.exe.manifest"
+  "Cinema_V3.pdb"
+  "libCinema_V3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cinema_V3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
